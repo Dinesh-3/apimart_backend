fa2d3cc6f15e8dc50df8960b31953c3108d49c28
Dinesh I <dineshiyyandhurai@gmail.com>
2021-10-26 16:15:10 +0530

    java version fix

--- src/main/java/com/codingmart/api_mart/configuration/SystemConfig.java
+++ src/main/java/com/codingmart/api_mart/configuration/SystemConfig.java
@@ -6,7 +6,13 @@ import java.util.List;
 import java.util.Set;
 
 public class SystemConfig {
-    private static final Set<String> reservedCollection = Set.of("user", "verification", "table");
+    private static final Set<String> reservedCollection = new HashSet<>();
+
+    static {
+        reservedCollection.add("user");
+        reservedCollection.add("verification");
+        reservedCollection.add("table");
+    }
 
     public static Set<String> getReservedCollections(){
         return reservedCollection;
